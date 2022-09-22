d339f7e25b6211e014144e889749c3237a8701fe
dlrkdmsdkdld <dlrkdmsdkdld@naver.com>
2022-09-22 21:01:23 +0900

    Feat : 로그인 가입하기 버튼있는 맨 처음 액티비티 구현 , sp->dp로 변환

--- Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/LoginActivity.kt
+++ Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/LoginActivity.kt
@@ -2,11 +2,18 @@ package kr.ac.kpu.ce2017154024.mytamin.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kpu.ce2017154024.mytamin.R
+import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityLoginBinding
+import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityMainBinding
+import kr.ac.kpu.ce2017154024.mytamin.utils.Constant
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var mbinding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mbinding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
+        Log.d(Constant.TAG,"LoginActivity onCreate")
     }
 }
---
+++ Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/firstActivity.kt
@@ -0,0 +1,42 @@
+package kr.ac.kpu.ce2017154024.mytamin.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_first.*
+import kr.ac.kpu.ce2017154024.mytamin.R
+import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityFirstBinding
+import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityLoginBinding
+import kr.ac.kpu.ce2017154024.mytamin.utils.Constant.TAG
+
+class firstActivity : AppCompatActivity(),View.OnClickListener {
+    private lateinit var mBinding: ActivityFirstBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        Log.d(TAG,"firstActivity onCreate")
+        first_join_btn.setOnClickListener(this)
+        first_login_btn.setOnClickListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"firstActivity onDestroy")
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0){
+            first_join_btn ->{
+                val intent = Intent(applicationContext,LoginActivity::class.java)
+                startActivity(intent)
+            }
+            first_login_btn ->{
+                val intent = Intent(applicationContext,LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
