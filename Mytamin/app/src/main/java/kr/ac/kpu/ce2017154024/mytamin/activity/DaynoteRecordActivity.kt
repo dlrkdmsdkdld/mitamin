44e80460f8e1095ba9b587b731c2af6f4c339ef8
dlrkdmsdkdld <dlrkdmsdkdld@naver.com>
2022-10-19 16:57:58 +0900

    feat: 데이노트 작성 액티비티 , 프래그먼트 화면구현

--- Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/DaynoteRecordActivity.kt
+++ Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/DaynoteRecordActivity.kt
@@ -2,10 +2,25 @@ package kr.ac.kpu.ce2017154024.mytamin.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kr.ac.kpu.ce2017154024.mytamin.R
+import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityMainBinding
+import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityRecordDaynoteBinding
 
 class DaynoteRecordActivity : AppCompatActivity() {
+    private lateinit var mbinding: ActivityRecordDaynoteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_daynote_record)
+        mbinding = ActivityRecordDaynoteBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
+
+        //네비게이션들을 담는 호스트
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.record_container) as NavHostFragment
+
+        //네비게이션 컨트롤러 가져옴
+        val navController = navHostFragment.navController
+
     }
 }
--- Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/RecordDaynoteActivity.kt
+++ Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/RecordDaynoteActivity.kt
@@ -2,16 +2,30 @@ package kr.ac.kpu.ce2017154024.mytamin.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityMydayBinding
 import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityRecordDaynoteBinding
 
-class RecordDaynoteActivity : AppCompatActivity() {
+class RecordDaynoteActivity : AppCompatActivity(),View.OnClickListener {
     private lateinit var mbinding: ActivityRecordDaynoteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mbinding= ActivityRecordDaynoteBinding.inflate(layoutInflater)
         setContentView(mbinding.root)
+        mbinding?.recordBackBtn.setOnClickListener(this)
 
 
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0){
+            mbinding?.recordBackBtn ->{
+                onBackPressed()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
