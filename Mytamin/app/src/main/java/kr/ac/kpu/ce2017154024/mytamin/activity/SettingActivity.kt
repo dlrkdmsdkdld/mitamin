b60106e7c083fa185e2746e252b378a2ffe63bdf
dlrkdmsdkdld <dlrkdmsdkdld@naver.com>
2022-10-28 03:50:30 +0900

    feat : 설정 화면 구현

---
+++ Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/SettingActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kpu.ce2017154024.mytamin.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
