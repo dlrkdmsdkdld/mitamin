87757955977cde028b4c741fcdb787594676d6ba
dlrkdmsdkdld <dlrkdmsdkdld@naver.com>
2022-11-08 00:04:22 +0900

    feat : 주간 마이타민스크롤 기능 api 구현

--- Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/WeeklyActivity.kt
+++ Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/activity/WeeklyActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kpu.ce2017154024.mytamin.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.haibin.calendarview.Calendar
@@ -11,7 +12,9 @@ import kr.ac.kpu.ce2017154024.mytamin.databinding.ActivityWeeklyBinding
 import kr.ac.kpu.ce2017154024.mytamin.model.dayMytamin
 import kr.ac.kpu.ce2017154024.mytamin.retrofit.token.HistoryRetrofitManager
 import kr.ac.kpu.ce2017154024.mytamin.retrofit.token.HomeRetrofitManager
+import kr.ac.kpu.ce2017154024.mytamin.utils.Constant.TAG
 import kr.ac.kpu.ce2017154024.mytamin.utils.getSchemeCalendar
+import kr.ac.kpu.ce2017154024.mytamin.utils.parseIntToMonth
 
 class WeeklyActivity : AppCompatActivity() {
     private lateinit var mbinding : ActivityWeeklyBinding
@@ -21,131 +24,99 @@ class WeeklyActivity : AppCompatActivity() {
         mbinding= ActivityWeeklyBinding.inflate(layoutInflater)
         setContentView(mbinding.root)
 
-        mbinding?.weeklyCalendar.apply {
 
-            this.setSelectStartCalendar(2022,11,1)
-        }
-        //mbinding?.weeklyCalendarLayout2.setDate(1570668133353L)
-//        mbinding?.weeklyCalendar.curMonth =1
-//        mbinding?.weeklyCalendar.set
         val day = intent.getStringExtra("day")
         day?.let {
-            HistoryRetrofitManager.instance.getWeekMytamin(day){responseStatus, ArraydayMytamins ->
-                ArraydayMytamins?.let {
-                    this.ArraydayMytamin = ArraydayMytamins
-                    val y = mbinding?.weeklyCalendar.curYear
-                    val m = mbinding?.weeklyCalendar.curMonth
-                    val mapdata: MutableMap<String, Calendar> = mutableMapOf()
-                    ArraydayMytamins.forEach {
-                            val day = it.day.toInt()
-                            it.report?.let {
-                                when(it.mentalConditionCode){
-                                    1->{
-                                        mapdata.put(
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.Gray),"").toString(),
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.Gray),"")
-                                        )
-                                    }
-                                    2->{
-                                        mapdata.put(
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.primary),"").toString(),
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.primary),"")
-                                        )
-                                    }
-                                    3->{
-                                        mapdata.put(
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.LawnGreen),"").toString(),
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.LawnGreen),"")
-                                        )
-                                    }
-                                    4->{
-                                        mapdata.put(
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.subBlue),"").toString(),
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.subBlue),"")
-                                        )
-                                    }
-                                    5->{
-                                        mapdata.put(
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.layoutYellow),"").toString(),
-                                            getSchemeCalendar(y,m,day,
-                                                ContextCompat.getColor(this, R.color.layoutYellow),"")
-                                        )
-                                    }
-                                    else ->{}
-
-
-                                }
-                        }
-                    }
-                    mbinding?.weeklyCalendar.setSchemeDate(mapdata)
-                }
-            }
+            var dayDate = it.split(".")
+            Log.d(TAG,"dayDate ->$dayDate")
+            mbinding?.weeklyCalendar.scrollToCalendar(dayDate[0].toInt(),dayDate[1].toInt(),dayDate[2].toInt())
+            setSchema(null,it, dayDate[1].toInt() ,dayDate[0].toInt())
+            mbinding?.weeklyTimeText.text = "${mbinding?.weeklyCalendar.curYear}년 ${mbinding?.weeklyCalendar.curMonth}월"
         }
         selectCalend()
+        mbinding?.weeklyCalendar.setOnWeekChangeListener(object :CalendarView.OnWeekChangeListener{
+            override fun onWeekChange(weekCalendars: MutableList<Calendar>?) {
+                val nowWeek = weekCalendars?.get(1)
+                mbinding?.weeklyTimeText.text = "${nowWeek?.year}년 ${nowWeek?.month}월"
+                weekCalendars?.let { setSchema(it,"${nowWeek?.year}.${nowWeek?.month.parseIntToMonth()}.${nowWeek?.day.parseIntToMonth()}",nowWeek!!.month,nowWeek!!.year) }
+
+            }
+
+        })
+
 
 
 
     }
-    private fun retrunShema(ArraydayMytamins:ArrayList<dayMytamin>,code:Int,y:Int,m:Int,day:Int){
-        ArraydayMytamins.forEach {
-            it.takeAt?.let {
-                it[0-1]
-            }
-        }
-        val mapdata: MutableMap<String, Calendar> = mutableMapOf()
-        when(code){
-            1->{
-                mapdata.put(
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.Gray),"").toString(),
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.Gray),"")
-                )
-            }
-            2->{
-                mapdata.put(
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.primary),"").toString(),
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.primary),"")
-                )
-            }
-            3->{
-                mapdata.put(
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.LawnGreen),"").toString(),
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.LawnGreen),"")
-                )
-            }
-            4->{
-                mapdata.put(
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.subBlue),"").toString(),
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.subBlue),"")
-                )
-            }
-            5->{
-                mapdata.put(
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.layoutYellow),"").toString(),
-                    getSchemeCalendar(y,m,day,
-                        ContextCompat.getColor(this, R.color.layoutYellow),"")
-                )
-            }
-            else ->{}
+    private fun setSchema(date:MutableList<Calendar>?,day:String,mont:Int,Year:Int){
+        HistoryRetrofitManager.instance.getWeekMytamin(day){responseStatus, ArraydayMytamins ->
+            ArraydayMytamins?.let {
+                this.ArraydayMytamin = ArraydayMytamins
+//                val y = mbinding?.weeklyCalendar.curYear
+//                val m = mbinding?.weeklyCalendar.curMonth
+                var count = 0
+                val mapdata: MutableMap<String, Calendar> = mutableMapOf()
+                ArraydayMytamins.forEach {
+                    val day = it.day.toInt()
+                    var y = Year
+                    var m = mont
+                    date?.let {
+                         y =  date.get(count).year
+                         m = date.get(count).month
+                    }
+
+                    it.report?.let {
+                        when(it.mentalConditionCode){
+                            1->{
+                                mapdata.put(
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.Gray),"").toString(),
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.Gray),"")
+                                )
+                            }
+                            2->{
+                                mapdata.put(
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.primary),"").toString(),
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.primary),"")
+                                )
+                            }
+                            3->{
+                                mapdata.put(
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.LawnGreen),"").toString(),
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.LawnGreen),"")
+                                )
+                            }
+                            4->{
+                                mapdata.put(
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.subBlue),"").toString(),
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.subBlue),"")
+                                )
+                            }
+                            5->{
+                                mapdata.put(
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.layoutYellow),"").toString(),
+                                    getSchemeCalendar(y,m,day,
+                                        ContextCompat.getColor(this, R.color.layoutYellow),"")
+                                )
+                            }
+                            else ->{}
 
 
+                        }
+
+                    }
+                    count+=1
+                }
+                mbinding?.weeklyCalendar.setSchemeDate(mapdata)
+            }
         }
     }
     private fun selectCalend(){
@@ -173,6 +144,12 @@ class WeeklyActivity : AppCompatActivity() {
     }
     private fun setData(data:dayMytamin){
         data.takeAt?.let { mbinding?.weeklyYesDate.text = it }
+        if (data.report == null){
+            mbinding?.yesMytaminFeelingTag.text = null
+            mbinding?.yesMytaminMentalConditionMsg.text =null
+            mbinding?.yesmytaminTodayReport.text=null
+            mbinding?.yesMytaminImage.setImageResource(R.drawable.ic_x_box)
+        }
         data.report?.let {
             mbinding?.yesMytaminFeelingTag.text = it.feelingTag
             mbinding?.yesMytaminMentalConditionMsg.text = it.mentalCondition
@@ -185,6 +162,10 @@ class WeeklyActivity : AppCompatActivity() {
                 5 ->{mbinding?.yesMytaminImage.setImageResource(R.drawable.ic_so_good)}
             }
 
+        }
+        if (data.care == null){
+            mbinding?.yesmytaminCareMsg1.text = ""
+            mbinding?.yesmytaminCareMsg2.text = ""
         }
         data.care?.let {
             mbinding?.yesmytaminCareMsg1.text = it.careMsg1
