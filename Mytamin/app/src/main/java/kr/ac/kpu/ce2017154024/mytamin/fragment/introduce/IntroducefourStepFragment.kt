653c7111dba451f00f2a9c8187a407a41aef90ed
dlrkdmsdkdld <dlrkdmsdkdld@naver.com>
2022-11-09 03:37:12 +0900

    feat: introduce UI 개선 및 커스텀 numberPicker적용

--- Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/fragment/introduce/IntroducefourStepFragment.kt
+++ Mytamin/app/src/main/java/kr/ac/kpu/ce2017154024/mytamin/fragment/introduce/IntroducefourStepFragment.kt
@@ -16,12 +16,18 @@ import kr.ac.kpu.ce2017154024.mytamin.utils.Constant.TAG
 
 class IntroducefourStepFragment : Fragment() {
     private var mBinding : FragmentIntroducefourStepBinding?=null
+    private var hour =1
+    private var minute =5
+    private var AMorPM =12
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentIntroducefourStepBinding.inflate(inflater,container,false)
         mBinding =binding
+        (activity as IntroduceActivity).submitTime(hour+AMorPM,minute)
+
         Log.d(Constant.TAG,"IntroducefourStepFragment onCreateView")
 
 
@@ -30,13 +36,9 @@ class IntroducefourStepFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        inputTime()
+        numberPickerListener()
 
-        val datePicker = introduce_timePicker
-        datePicker.setOnTimeChangedListener { timePicker, i, i2 ->
-            (activity as IntroduceActivity).submitTime(i,i2)
-            Log.d(TAG,"${i}시 ${i2}분")
-
-        }
         introduce_start_btn.setOnClickListener {
             val email = (activity as IntroduceActivity).getemailData()
             val password = (activity as IntroduceActivity).getpasswordData()
@@ -55,6 +57,58 @@ class IntroducefourStepFragment : Fragment() {
         super.onDestroyView()
         Log.d(Constant.TAG,"IntroducefourStepFragment onDestroyView")
     }
+    private fun inputTime(){
+        val hourdata = arrayOf("00시","01시","02시" , "03시","04시","05시","06시","07시","08시","09시","10시","11시")
+        mBinding!!.introduceRecordHour.apply {
+            maxValue =11
+            minValue = 0
+            displayedValues = hourdata
+        }
+        val minutedata = arrayOf("00분","05분","10분" , "15분","20분","25분","30분","35분","40분","45분","50분","55분")
+        mBinding!!.introduceRecordTime.apply {
+            maxValue =11
+            minValue = 0
+            displayedValues = minutedata
+        }
+        val timedata = arrayOf("오후","오전")
+        mBinding!!.introduceRecordNight.apply {
+            minValue=10
+            maxValue=11
+            displayedValues = timedata
+        }
 
+
+
+    }
+    private fun numberPickerListener(){
+        mBinding?.introduceRecordNight?.setOnValueChangedListener { picker, oldVal, newVal ->
+            if (newVal ==10){
+                Log.d(TAG,"bottomRecordNight  오후선택  ")
+                AMorPM=12
+            }else{
+                Log.d(TAG,"bottomRecordNight  오전선택  ")
+                AMorPM=0
+
+            }
+            Log.d(TAG, " 현재 선택 시간 : ${hour+AMorPM}시 ${minute}분")
+            (activity as IntroduceActivity).submitTime(hour+AMorPM,minute)
+
+        }
+        mBinding?.introduceRecordHour?.setOnValueChangedListener { picker, oldVal, newVal ->
+            hour = newVal
+            Log.d(TAG,"bottomRecordHour  시간 :$newVal  ")
+            Log.d(TAG, " 현재 선택 시간 : ${hour+AMorPM}시 ${minute}분")
+            (activity as IntroduceActivity).submitTime(hour+AMorPM,minute)
+        }
+        mBinding?.introduceRecordTime?.setOnValueChangedListener{ picker, oldVal, newVal ->
+            Log.d(TAG,"bottomRecordTime  분 :${newVal*5}  ")
+            minute = newVal*5
+            Log.d(TAG, " 현재 선택 시간 : ${hour+AMorPM}시 ${minute}분")
+            (activity as IntroduceActivity).submitTime(hour+AMorPM,minute)
+        }
+
+
+
+    }
 
 }
